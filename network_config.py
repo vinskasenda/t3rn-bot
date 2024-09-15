7a261542b6706e1957b0a962c65582513984a034
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-09-15 15:31:32 +0700

    Create network_config.py

---
+++ network_config.py
@@ -0,0 +1,24 @@
+# network_config.py
+
+networks = {
+    'Arbitrum Sepolia': {
+        'rpc_url': 'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+        'chain_id': 421614,
+        'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
+    },
+    'OP Sepolia': {
+        'rpc_url': 'https://sepolia.optimism.io',
+        'chain_id': 11155420,
+        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
+    },
+    'Blast Sepolia': {
+        'rpc_url': 'https://base-sepolia-rpc.publicnode.com',
+        'chain_id': 84532,
+        'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'
+    },
+    'Base Sepolia': {
+        'rpc_url': 'https://base-sepolia-rpc.publicnode.com',
+        'chain_id': 84532,
+        'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'
+    }
+}
