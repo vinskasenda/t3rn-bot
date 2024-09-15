830322b1c8c3948522ebe4803fbe2539d28de91c
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-09-15 15:32:58 +0700

    Create keys_and_addresses.py

---
+++ keys_and_addresses.py
@@ -0,0 +1,18 @@
+# keys_and_addresses.py
+private_keys = [
+    'YOUR PRIVATE KEY WALLET 1',
+    'YOUR PRIVATE KEY WALLET 2',
+    'YOUR PRIVATE KEY WALLET 3'
+]
+
+my_addresses = [
+    'ADDRESS 1',  # Alamat address EVM / Your EVM Address
+    'ADDRESS 2',
+    'ADDRESS 3'
+]
+
+labels = [
+    'Wallet 1',
+    'Wallet 2',
+    'Wallet 3'
+]
