777aa42caede390e66b31a2d97193bc96b4f5b50
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-09-20 11:45:58 +0700

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -7,7 +7,7 @@ networks = {
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
     'OP Sepolia': {
-        'rpc_url': 'https://sepolia.optimism.io',
+        'rpc_url': 'https://optimism-sepolia.blockpi.network/v1/rpc/public',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
