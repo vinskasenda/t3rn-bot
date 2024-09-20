d5ef6d26b85bc589368a8e242a7b7113b2b4d628
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-09-20 19:37:06 +0700

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,7 +2,7 @@
 
 networks = {
     'Arbitrum Sepolia': {
-        'rpc_url': 'https://endpoints.omniatech.io/v1/arbitrum/sepolia/public',
+        'rpc_url': 'https://sepolia-rollup.arbitrum.io/rpc',
         'chain_id': 421614,
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
